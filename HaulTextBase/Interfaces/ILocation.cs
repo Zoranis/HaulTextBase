f92381250d494c5f2822828ebe0b761b750f0b2e
Zoranis <zonisr@gmail.com>
2025-07-12 21:00:47 +0300

    Add project files.

---
+++ HaulTextBase/Interfaces/ILocation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaulTextBase.Interfaces
+{
+    internal class ILocation
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<IInteractable> Interactables { get; set; } = new List<IInteractable>();
+        
+    }
+}
