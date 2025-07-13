a9089e2e544ec851ebbf25c87f92b76f83eac5c2
Zoranis <Zoranis@users.noreply.github.com>
2025-07-13 17:56:39 +0300

    added gamemanager

---
+++ HaulTextBase/Game/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaulTextBase.Game
+{
+    internal class GameManager
+    {
+        public GameState? _gameState;
+
+        public void NewGame()
+        {
+            _gameState = new GameState();
+            _gameState._player = new Player();
+        }
+
+    }
+}
